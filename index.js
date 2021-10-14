1547245a876e644d630cacfc5da6e0762fd28c6f
Alex Klein <alex.klein4414@gmail.com>
2021-10-14 17:22:59 -0400

    Set Up Mongoose, finished Module 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 4000;
@@ -15,3 +16,9 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
+mongoose.connect('mongodb://localhost/productsdb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+mongoose.Promise = global.Promise;

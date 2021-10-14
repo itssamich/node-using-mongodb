9107013ce4f0ebae70aefa4c39f592ecef06cccd
Alex Klein <alex.klein4414@gmail.com>
2021-10-14 18:15:16 -0400

    Completed Module 4

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,7 @@
-import { addnewProduct } from "../controllers/controllers";
+import { addnewProduct, getProducts, getProductWithID } from "../controllers/controllers";
 
 export function routes(app){
     app.route('/products').post(addnewProduct);
+    app.route('/products').get(getProducts);
+    app.route('/products/:ProductID').get(getProductWithID);
 }
